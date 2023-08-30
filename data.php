ac2caa1e5d51ea84a4382813513136314c834c26
Kevin Muscara <kevinmuscara@Kevins-Mac-mini.local>
2023-08-30 19:34:06 -0400

    my part finished

--- data.php
+++ data.php
@@ -19,6 +19,16 @@ $data = [
         "university" => "Northern Kentucky University",
         "timeline" => "Expected 2026",
         "degree" => "BS, Applied Software Engineering"
+      ],
+      [
+        "university" => "Jessamine Career and Technology Center",
+        "timeline" => "May 2022",
+        "degree" => "Information Technology"
+      ],
+      [
+        "university" => "West Jessamine High School",
+        "timeline" => "May 2022",
+        "degree" => "Diploma"
       ]
     ],
     "skills" => [
@@ -32,9 +42,13 @@ $data = [
           "percent" => 80
         ],
         [
+          "name" => "Database",
+          "percent" => 80
+        ],
+        [
           "name" => "React",
           "percent" => 60
-        ]
+        ],
       ],
       "other" => ["Docker", "Python", "IAM Automation", "Computer Networking", "Project Management"],
     ],
@@ -76,13 +90,39 @@ $data = [
     ],
     "experiences" => [
       [
+        'position' => 'President',
+        'company' => 'Norse IoT',
+        'timeline' => 'January 2023 - Present',
+        'description' => 'Managed student led research and development of IoT applications. Planned and advertised all organization meetings and fundraising opportunities. Maintained and approved all organization purchase requests and budgets.',
+        'achievements' => [],
+        'tech' => [
+          'ESP32'
+        ]
+      ],
+      [
+        'position' => 'Chief Executive Officer',
+        'company' => 'Muscara Software Solutions',
+        'timeline' => 'April 2022 - Present',
+        'description' => "Engineered and implemented a educational technology application, actively empowering more than 8,000 students. Developed a suite of data analysis tools that automated student state testing results. Provided professional software consulting and testing to improve clients product reliability. Developed client landing pages and CMS dashboards to increase digital presence.",
+        'achievements' => ["Raised over $6,000 in operation funding"],
+        'tech' => ["Node.JS", 'Docker', 'Railway', 'MySQL', 'Deno', 'React', 'PostgreSQL', 'MariaDB'],
+      ],
+      [
         'position' => 'Support Engineer Intern',
         'company' => 'Identity Automation',
         'timeline' => 'May 2023 - August 2023',
-        'description' => 'Helped people',
-        'achievements' => ["maintained 15 minute response SLA"],
-        'tech' => ["Salesforce"],
-      ]
+        'description' => "Provided web-based application consulting and/or support. Performed in-depth analysis of functionality and usability concerns. Collaborated with client's IT personnel to comprehend complex issues. Diagnosed client SSO/SAML configurations and adeptly executed IAM remedies.",
+        'achievements' => ["Maintained 15 minute response SLA", "Exceeded monthly ticket quota."],
+        'tech' => ["Salesforce", "Slack"],
+      ],
+      [
+        'position' => 'Network Engineer Team Lead',
+        'company' => 'Jessamine County Schools',
+        'timeline' => 'June 2019 - May 2022',
+        'description' => "Performed training/onboarding of new student workers. Designed and implemented advanced dynamic routing configurations (OSPF, SPB, ISIS). Engineered automation scripts to improve job efficiency. Assisted in research and development of newer services and solutions. Installed, maintained, and configured Extreme/Avaya devices of the core network infrastructure. Managed Google Enterprise of over 12,000 Google devices, enrolling and deprovisioning when needed. Provided IAM maintenance for device and user profiles in Active Directory and Google.",
+        'achievements' => [],
+        'tech' => ["Avaya", "Slack", 'Extreme', 'Perl', 'Ruby', 'Python', 'Google Suite']
+      ],
     ]
   ]
 ];
