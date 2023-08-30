2110921fec23e2c7c20f052aa3bac74ca6785efd
Kevin Muscara <kevinmuscara@nku21r0315.hh.nku.edu>
2023-08-30 17:49:29 -0400

    finished just need to fill data

---
+++ data.php
@@ -0,0 +1,64 @@
+<?php
+$data = [
+  "Kevin" => [
+    "general" => [
+      "name" => "Kevin Muscara",
+      "title" => "Software Engineer",
+      "email" => "muscarak1@nku.edu",
+      "phone" => "+18595518787",
+      "summary" => "software engineer seeking a management position.",
+      "avatarURL" => "https://avatars.githubusercontent.com/u/29820047?v=4"
+    ],
+    "links" => [
+      "LinkedIn" => "kevinmuscara",
+      "GitHub" => "kevinmuscara",
+      "Website" => "kevin.cafe"
+    ],
+    "education" => [
+      [
+        "university" => "Northern Kentucky University",
+        "timeline" => "Expected 2026",
+        "degree" => "Bachelor of Science - BS, Applied Software Engineering"
+      ]
+    ],
+    "skills" => [
+      "main" => [
+        [
+          "name" => "JavaScript",
+          "percent" => 100
+        ],
+        [
+          "name" => "Node.JS",
+          "percent" => 100
+        ]
+      ],
+      "other" => ["Docker"],
+    ],
+    "awards" => [
+      [
+        "title" => "STLP Mobile App Challenge",
+        "description" => "Placed 1st in the NKU STLP Mobile App Challenge"
+      ]
+    ],
+    "languages" => ["English"],
+    "interests" => ["hiking", "programming", "distributed systems"],
+    "projects" => [
+      [
+        "title" => "Hallpass",
+        "description" => "Digital hallway management software used by over 8,000 students",
+        "image" => "https://hallpass.gg/seo.png",
+        "link" => "https://hallpass.gg",
+      ]
+    ],
+    "experiences" => [
+      [
+        'position' => 'Support Engineer Intern',
+        'company' => 'Identity Automation',
+        'timeline' => 'May 2023 - August 2023',
+        'description' => 'Helped people',
+        'achievements' => ["maintained 15 minute response SLA"],
+        'tech' => ["Salesforce"],
+      ]
+    ]
+  ]
+];
